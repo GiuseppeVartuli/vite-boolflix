bcc08331cd68a00039820ec27093838f75d3215e
GiuseppeVartuli <giuseppevartuli89@gmail.com>
2024-02-23 13:50:43 +0100

    add series

--- src/App.vue
+++ src/App.vue
@@ -13,27 +13,48 @@ export default {
   data() {
     return {
       movies: [],
+      series: [],
       searchText: "",
       store,
     };
   },
   computed: {},
   methods: {
-    searchFilms() {
+    searchUrl() {
       console.log("searchFilms chiamato");
       console.log(this.searchText);
 
       const text = this.searchText.split(" ");
       const titlePlus = text.join("+");
       console.log(titlePlus);
-      const urlMovie = `${store.api_movie_url}api_key=${store.api_key}&query=${titlePlus}`;
-      console.log(urlMovie);
+      const urlBoolflix = `${store.api_movie_url}api_key=${store.api_key}&query=${titlePlus}`;
+      console.log(urlBoolflix);
 
       axios
-        .get(urlMovie)
+        .get(urlBoolflix)
         .then((response) => {
           this.movies = response.data.results;
           console.log(this.movies);
+        })
+        .catch((error) => {
+          console.error("Errore nella richiesta:", error);
+        });
+    },
+    searchUrl() {
+      console.log("searchSeries chiamato");
+      console.log(this.searchText);
+
+      const text = this.searchText.split(" ");
+      const titlePlus = text.join("+");
+      console.log(titlePlus);
+      const urlBoolflix = `${store.api_series_url}api_key=${store.api_key}&query=${titlePlus}`;
+      console.log(urlBoolflix);
+
+      axios
+        .get(urlBoolflix)
+        .then((response) => {
+          this.series = response.data.results;
+          console.log(this.series);
         })
         .catch((error) => {
           console.error("Errore nella richiesta:", error);
@@ -56,7 +77,8 @@ export default {
         placeholder="Cerca Film o Serie"
         v-model="searchText"
       />
-      <button @click="searchFilms">Cerca</button>
+      <button @click="searchUrl">Cerca</button>
+
       <div class="movie" v-for="movie in movies">
         <img :src="`${store.api_img_url}w200${movie.poster_path}`" alt="" />
         <div class="description">
@@ -64,6 +86,16 @@ export default {
           <p class="original_title">{{ movie.original_title }}</p>
           <p class="language">{{ movie.original_language }}</p>
           <p class="vote">{{ movie.vote_average }}</p>
+        </div>
+      </div>
+
+      <div class="series" v-for="serieItem in series">
+        <img :src="`${store.api_img_url}w200${serieItem.poster_path}`" alt="" />
+        <div class="description">
+          <p class="title">{{ serieItem.title }}</p>
+          <p class="original_title">{{ serieItem.original_title }}</p>
+          <p class="language">{{ serieItem.original_language }}</p>
+          <p class="vote">{{ serieItem.vote_average }}</p>
         </div>
       </div>
     </div>
--- src/store.js
+++ src/store.js
@@ -4,4 +4,5 @@ export const store = reactive({
   api_movie_url: "https://api.themoviedb.org/3/search/movie?",
   api_key: "47cdb8689cbe8f286343e7e1d5ceab3f",
   api_img_url: "https://image.tmdb.org/t/p/",
+  api_series_url: "https://api.themoviedb.org/3/search/tv?",
 });
