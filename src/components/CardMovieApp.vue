16c7a6001c1165d7ec2a716f85c0e26bf7a131bf
GiuseppeVartuli <giuseppevartuli89@gmail.com>
2024-03-23 21:19:41 +0100

    fix code

--- src/components/CardMovieApp.vue
+++ src/components/CardMovieApp.vue
@@ -10,31 +10,6 @@ export default {
 };
 </script>
 
-<template>
-  <div class="movie" v-for="movie in store.movies">
-    <img :src="`${store.api_img_url}w342${movie.poster_path}`" alt="" />
-    <div class="description">
-      <p class="title">{{ movie.title }}</p>
-      <p class="original_title">{{ movie.original_title }}</p>
-      <p class="language">
-        <span
-          class="flag-icon"
-          :class="getLanguageFlagClass(movie.original_language)"
-        ></span>
-      </p>
-
-      <div v-if="movie">
-        <div v-if="movie">
-          <div v-for="index in 5" :key="index">
-            <i
-              class="fas fa-star"
-              :class="starsClass(index, movie.vote_average)"
-            ></i>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-</template>
+<template></template>
 
 <style scoped></style>
